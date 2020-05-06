2a7983ca8f14f038e2b601396b7a2ea7e3082b07
devcodeslash <elibrahimadiago@gmail.com>
2020-05-06 22:24:20 +0000

    Troisieme commit add contrib

--- src/main/java/com/atnb/africathenextbrain/AfricathenextbrainApplication.java
+++ src/main/java/com/atnb/africathenextbrain/AfricathenextbrainApplication.java
@@ -21,6 +21,7 @@ public class AfricathenextbrainApplication implements CommandLineRunner {
 
 		contributionRepository.save(new Contribution(null,"je suis africa","ibou","agriculture",1));
         contributionRepository.save(new Contribution(null,"Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum","ibou","agriculture",1));
+		contributionRepository.save(new Contribution(null,"Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum","Arame Diago","Science",1));
 
 		contributionRepository.findAll().forEach(p->{
 			System.out.println(p.toString());
